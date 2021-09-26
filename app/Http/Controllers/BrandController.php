61d82c06161b0bd6b1526da9bbb7e48849ffb092
luisbull <luisbull@hotmail.com>
2021-09-26 11:38:32 +0100

    Images save to Storage folder - then link to Public folder in order to dislplay them

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
@@ -24,7 +25,7 @@ class BrandController extends Controller
         $validated = $request->validate(
             [
                 'brand_name' => 'required|unique:brands|min:4',
-                'brand_image' => 'required|mimes:jpg, jpeg, png, svg, bpm|max:1048',
+                'brand_image' => 'required|mimes:jpg,jpeg,bmp,png,svg|max:1048',
             ],
             [
                 'brand_name.required' => 'Please enter brand name', // here can be customised text instead of default message
@@ -33,5 +34,25 @@ class BrandController extends Controller
         );
 
         $brand_image = $request->file('brand_image');
+
+        // $name_gen = hexdec(uniqid());
+        // $img_ext = strtolower($brand_image->getClientOriginalExtension());
+        // $img_name = $name_gen.'.'.$img_ext;
+        $up_loacation = 'image/brand';
+        // $last_img = $up_loacation.$img_name;
+
+        // $brand_image->move($up_loacation, $img_name);
+        // $brand_image->move($up_loacation, $img_name);
+        
+
+        Brand::insert([
+            'brand_name' => $request->brand_name,
+            'brand_image' => $brand_image->store($up_loacation, 'public'),
+            // store('uploads', 'public')
+            'created_at' => Carbon::now()
+        ]);
+
+        return redirect()->back()->with('success', 'Brand Inserted Successfully');
+
     }
 }

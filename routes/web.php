2826b698d5ae7d8475fdb06f006c4f0e06b9df82
luisbull <luisbull@hotmail.com>
2021-10-09 12:54:34 +0100

    Verify email feature implemented

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,10 @@ use Illuminate\Support\Facades\DB; // use for Query Builder
 |
 */
 
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
 Route::get('/', function () {
     return view('welcome');
 });

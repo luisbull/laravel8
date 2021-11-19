73322781ea44beb954fd3776753390eec8f24fbf
luisbull <luisbull@hotmail.com>
2021-11-19 00:02:25 +0000

    update function implemented in controller

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -6,6 +6,7 @@ use App\Models\Portfolio;
 use Illuminate\Http\Request;
 
 use Carbon\Carbon;
+use Illuminate\Support\Facades\File;
 use Intervention\Image\Facades\Image;
 
 class PortfolioController extends Controller
@@ -77,6 +78,72 @@ class PortfolioController extends Controller
         $categories_array = array_diff($categories_array, array($portfolio->category));
         return view('admin.pics.edit', compact('portfolio', 'categories_array'));
     }
+
+
+    ////////////
+    // UPDATE //
+    ////////////
+    public function Update(Request $request, $id){
+        $validated = $request->validate(
+            [
+                'portfolio_title' => 'required|min:4',
+                'portfolio_category' => 'required',
+                // 'portfolio_image' => 'required|mimes:jpg,jpeg,bmp,png,svg|max:1048',
+            ],
+            [
+                'portfolio_title.required' => 'Please enter portfolio title', // here can be customised text instead of default message
+                //  'title.min' => 'Min 20' // here can be customised text instead of default message
+                // 'portfolio_category.required' => 'necesitas las descripcion'
+                //  'image.mimes' => 'Only jpg,jpeg,bmp,png,svg accepted' // here can be customised text instead of default message
+            ], 
+        );
+
+        $portfolio_image = $request->file('portfolio_image');
+        
+        $up_location = 'image/allpictures';
+        
+        $portfolio_entry = Portfolio::find($id);
+        $old_image = public_path('storage/'.$portfolio_entry->image);
+        
+        if($portfolio_image){
+            if(File::exists($old_image)){
+                File::delete($old_image);
+                
+                Portfolio::find($id)->update([
+                    'title' => $request->portfolio_title,
+                    'category' => $request->portfolio_category,
+                    'image' => $portfolio_image->store($up_location, 'public'),
+                    // store('uploads', 'public')
+                    'updated_at' => Carbon::now()
+                ]);
+    
+                return redirect()->back()->with('success', 'Slider Updated Successfully');    
+            
+            }else{
+                Portfolio::find($id)->update([
+                    'title' => $request->portfolio_title,
+                    'category' => $request->portfolio_category,
+                    'image' => $portfolio_image->store($up_location, 'public'),
+                    // store('uploads', 'public')
+                    'updated_at' => Carbon::now()
+                ]);
+
+                return redirect()->back()->with('success', 'Slider Updated Successfully');
+            }
+        }else{
+            Portfolio::find($id)->update([
+                'title' => $request->portfolio_title,
+                'category' => $request->portfolio_category,
+                // 'image' => $brand_image->store($up_loacation, 'public'),
+                // store('uploads', 'public')
+                // 'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+
+            return redirect()->back()->with('success', 'Category Updated Successfully');
+        }
+    }
+
 
     ///////////////////
     // FRONTEND PAGE //

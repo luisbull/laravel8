5da976848d857e5592839eb91467654b2f94a38c
luisbull <luisbull@hotmail.com>
2021-11-30 20:50:14 +0000

    typo error

--- resources/views/admin/user/user_profile.blade.php
+++ resources/views/admin/user/user_profile.blade.php
@@ -40,7 +40,7 @@
                 @enderror
               </div>
               <div class="form-group">
-                <label for="profileImage">Profile Email</label>
+                <label for="profileImage">Profile Image</label>
                 <input type="file" name="profile_image" class="form-control" id="profileImage"  value="{{ asset('storage/'.$userProfile->profile_photo_path) }}">
                 @error('profile_image')
                 <span class="text-danger">{{ $message }}</span>

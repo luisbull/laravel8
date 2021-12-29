44cbd5a73d810b4e255d552539ada614181966e1
luisbull <luisbull@hotmail.com>
2021-12-29 16:48:44 +0000

    Loads of changes:
    - All code for PortfolioCategory implemented here. If needed code would be passed to PortfolioCategoryController.php
    
    - some existing code modified to make it work under new table structure.
    
    - tidied up code

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Portfolio;
+use App\Models\PortfolioCategory;
 use Illuminate\Http\Request;
 
 use Carbon\Carbon;
@@ -15,78 +16,84 @@ class PortfolioController extends Controller
     // {
     //     $this->middleware('auth');
     // }
-    
+
+
     /////////////////////
     // READ (load ALL) //
     /////////////////////
-    public function AllImages(){
+    public function AllImages()
+    {
         $allImages = Portfolio::all();
-        $categories_array = array("app","card","web");
-        return view('admin.portfolio.index', compact('allImages', 'categories_array'));
+        $categories = PortfolioCategory::all();
+
+        return view('admin.portfolio.index', compact('allImages', 'categories'));
     }
+
 
     ////////////
     // CREATE //
     ////////////
-    public function StoreImages(Request $request){
+    public function StoreImages(Request $request)
+    {
+
         $validated = $request->validate(
             [
-        //         'brand_name' => 'required|unique:brands|min:4',
+                //         'brand_name' => 'required|unique:brands|min:4',
                 'image' => 'required',
                 'portfolio_title' => 'required',
                 'portfolio_category' => 'required',
             ],
             [
                 // 'image.required' => 'POR AKI', // here can be customised text instead of default message
-        //         //  'brand_name.min' => 'Min 20' // here can be customised text instead of default message
-            ], 
+                //         //  'brand_name.min' => 'Min 20' // here can be customised text instead of default message
+            ],
         );
 
-        
-
         $array_of_images = $request->file('image');
-        $up_location = 'image/allpictures';  
+        $up_location = 'image/allpictures';
 
-        foreach($array_of_images as $single_image){
-            // dd($array_of_images);
+        foreach ($array_of_images as $single_image) {
             // keep ratio height and width
-            Image::make($single_image)->resize(null, 200, function($constraint) {
+            Image::make($single_image)->resize(null, 200, function ($constraint) {
                 $constraint->aspectRatio();
             })->save();
-    
+
             Portfolio::insert([
                 // 'brand_name' => $request->brand_name.'.'.$single_image->extension(),
                 'image' => $single_image->store($up_location, 'public'), //for this to work remember, run in terminal: php artisan storage:link //
-                'category' => $request->portfolio_category,
+                'cat_id' => (int)$request->portfolio_category,
+                'cat_name' => PortfolioCategory::find($request->portfolio_category)->name,
                 'title' => $request->portfolio_title,
                 'created_at' => Carbon::now()
             ]);
-
         } //end foreach
-
 
         return redirect()->back()->with('success', 'Images Inserted Successfully');
     }
 
+
     //////////
     // EDIT //
     //////////
-    public function Edit($id){
+    public function Edit($id)
+    {
         $portfolio = Portfolio::find($id);
-        $categories_array = array("app","card","web");
-        $categories_array = array_diff($categories_array, array($portfolio->category));
-        return view('admin.portfolio.edit', compact('portfolio', 'categories_array'));
+        $categories = PortfolioCategory::all();
+        // $categories = array_diff(array($categories), array($portfolio->category));
+        return view('admin.portfolio.edit', compact('portfolio', 'categories'));
     }
 
 
     ////////////
     // UPDATE //
     ////////////
-    public function Update(Request $request, $id){
+    public function Update(Request $request, $id)
+    {
+
         $validated = $request->validate(
             [
                 'portfolio_title' => 'required|min:4',
-                'portfolio_category' => 'required',
+                // 'portfolio_category' => 'required',
                 // 'portfolio_image' => 'required|mimes:jpg,jpeg,bmp,png,svg|max:1048',
             ],
             [
@@ -94,34 +101,36 @@ class PortfolioController extends Controller
                 //  'title.min' => 'Min 20' // here can be customised text instead of default message
                 // 'portfolio_category.required' => 'necesitas las descripcion'
                 //  'image.mimes' => 'Only jpg,jpeg,bmp,png,svg accepted' // here can be customised text instead of default message
-            ], 
+            ],
         );
 
         $portfolio_image = $request->file('portfolio_image');
-        
+
         $up_location = 'image/allpictures';
-        
+
         $portfolio_entry = Portfolio::find($id);
-        $old_image = public_path('storage/'.$portfolio_entry->image);
-        
-        if($portfolio_image){
-            if(File::exists($old_image)){
+        $old_image = public_path('storage/' . $portfolio_entry->image);
+
+
+        if ($portfolio_image) {
+            if (File::exists($old_image)) {
                 File::delete($old_image);
-                
+
                 Portfolio::find($id)->update([
                     'title' => $request->portfolio_title,
-                    'category' => $request->portfolio_category,
+                    'cat_id' => $request->portfolio_category,
+                    'cat_name' => PortfolioCategory::find($request->portfolio_category)->name,
                     'image' => $portfolio_image->store($up_location, 'public'),
                     // store('uploads', 'public')
                     'updated_at' => Carbon::now()
                 ]);
-    
-                return redirect()->back()->with('success', 'Slider Updated Successfully');    
-            
-            }else{
+
+                return redirect()->back()->with('success', 'Slider Updated Successfully');
+            } else {
                 Portfolio::find($id)->update([
                     'title' => $request->portfolio_title,
-                    'category' => $request->portfolio_category,
+                    'cat_id' => $request->portfolio_category,
+                    'cat_name' => PortfolioCategory::find($request->portfolio_category)->name,
                     'image' => $portfolio_image->store($up_location, 'public'),
                     // store('uploads', 'public')
                     'updated_at' => Carbon::now()
@@ -129,41 +138,137 @@ class PortfolioController extends Controller
 
                 return redirect()->back()->with('success', 'Slider Updated Successfully');
             }
-        }else{
+        } else {
+
             Portfolio::find($id)->update([
                 'title' => $request->portfolio_title,
-                'category' => $request->portfolio_category,
+                'cat_id' => $request->portfolio_category,
+                'cat_name' => PortfolioCategory::find($request->portfolio_category)->name,
                 // 'image' => $brand_image->store($up_loacation, 'public'),
                 // store('uploads', 'public')
                 // 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ]);
 
-            return redirect()->back()->with('success', 'Category Updated Successfully');
+            return redirect()->back()->with('success', 'Slider Updated Successfully');
         }
     }
+
 
     ////////////
     // DELETE //
     ////////////
-    public function Delete($id){
+    public function Delete($id)
+    {
         $portfolio_entry = Portfolio::find($id);
-        $old_image = public_path('storage/'.$portfolio_entry->image);
+        $old_image = public_path('storage/' . $portfolio_entry->image);
 
         File::delete($old_image);
         Portfolio::find($id)->delete();
 
-        return redirect()->back()->with('success', 'Project Deleted Successfully' );
+        return redirect()->back()->with('success', 'Project Deleted Successfully');
     }
 
 
     ///////////////////
     // FRONTEND PAGE //
     ///////////////////
-    public function Portfolio(){
+    public function Portfolio()
+    {
         $allImages = Portfolio::all();
-        $categories_array = array("app","card","web");
-        return view('layouts.pages.portfolio', compact('allImages', 'categories_array'));
+        $categories = PortfolioCategory::all();
+        return view('layouts.pages.portfolio', compact('allImages', 'categories'));
+    }
+
+
+    //////////////////
+    // ADD CATEGORY //
+    //////////////////
+    public function AddCategory()
+    {
+        $allImages = Portfolio::all();
+        $categories = PortfolioCategory::all();
+
+        return view('admin.portfolio.addCategory');
+    }
+
+
+    ////////////////////
+    // STORE CATEGORY //
+    ////////////////////
+    public function StoreCategory(Request $request)
+    {
+        $validated = $request->validate(
+            [
+                'portfolio_category' => 'required|min:2',
+            ],
+        );
+
+        PortfolioCategory::insert([
+            'name' => $request->portfolio_category,
+            'created_at' => Carbon::now()
+        ]);
+
+        return redirect()->route('all.multiImage')->with('success', 'Category Created Successfully');
+    }
+
+
+    ///////////////////
+    // EDIT CATEGORY //
+    ///////////////////
+    public function EditCategory($id)
+    {
+        // $portfolio = Portfolio::find($id);
+        $categories = PortfolioCategory::find($id);
+        // $categories = PortfolioCategory::all();
+        // $categories = array_diff(array($categories), array($portfolio->category));
+        return view('admin.portfolio.editCategory', compact('categories'));
+    }
+
+
+    /////////////////////
+    // UPDATE CATEGORY //
+    /////////////////////
+    public function UpdateCategory(Request $request, $id)
+    {
+        try {
+            $validated = $request->validate(
+                [
+                    'portfolio_category' => 'required|min:2',
+                ]
+            );
+    
+            PortfolioCategory::find($id)->update([
+                'name' => $request->portfolio_category,
+                'updated_at' => Carbon::now()
+            ]);
+            Portfolio::where('cat_id', $id)->update([
+                'cat_name' => $request->portfolio_category
+            ]);
+            // $users = Portfolio::where('cat_id', $id)->get();
+    
+            return redirect()->route('all.multiImage')->with('success', 'Category Updated Successfully');
+
+        } catch(\Illuminate\Database\QueryException $ex) {
+            return redirect()->route('all.multiImage')->with('error', 'Project category '. $request->portfolio_category . ' already exits. Please use different name');
+        }
+        
+    }
+
+
+    ////////////
+    // DELETE //
+    ////////////
+    public function DeleteCategory($id)
+    {
+        $category  = PortfolioCategory::find($id);
+        // dd($category);
+        try {
+            PortfolioCategory::find($id)->delete();
+            return redirect()->back()->with('success', 'Project Category Deleted Successfully');
+        } catch (\Illuminate\Database\QueryException $ex) {
+            return redirect()->back()->with('error', 'Project category '. $category->name . ' can\'t be Deleted. There are projects assigned to this category');
+        }
     }
 
 }
